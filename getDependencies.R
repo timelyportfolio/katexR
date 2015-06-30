e724ef311f154ed5fc3e3c3141b2d70438a37d78
timelyportfolio <kent.russell@timelyportfolio.com>
2015-06-30 10:51:18 -0500

    add regex to remove woff2 from css and rename output and change yaml to point there

--- getDependencies.R
+++ getDependencies.R
@@ -8,3 +8,18 @@ unzip(tf,exdir = "./inst/htmlwidgets/lib")
 
 
 # add delete woff2 from css and delete woff2 files
+readLines(
+  "inst/htmlwidgets/lib/katex/katex.min.css"
+) %>>%
+  (
+    gsub(
+      x = .
+      ,pattern = "(url)\\(fonts/[a-zA-Z0-9-_]*\\.woff2\\)\\s?(format\\('woff2'\\),)"   #*/[a-z][A-Z\\-_]*#\\.woff2\\)"#[\\s]format\\('woff2'\\),"
+      ,perl = T
+      ,replacement = ""
+    )
+  ) %>>%
+  (
+    cat(., file="inst/htmlwidgets/lib/katex/katex_nowoff2.min.css")
+  )
+  
